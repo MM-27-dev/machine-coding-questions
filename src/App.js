2255159655d4e7e8b5f8f409efd4cc04bb86deb4
Monika <monikamuniraju27@gmail.com>
2025-03-11 01:42:20 +0000

    Updated project

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ export default function App() {
   return (
     <div className="App">
       <h1>Hello CodeSandbox</h1>
-      <h2>Start editing to see some magic mnbjh happen!</h2>
+      <h2>Start editing to see some magic kihoi98u mnbjh happenmm!</h2>
     </div>
   );
 }
